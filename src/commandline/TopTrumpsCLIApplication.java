e3f92b2150dd174a91ac69357fb468fbe0d8e983
Emily Murphy <2087350m@student.gla.ac.uk>
2020-01-24 14:53:44 +0000

    command line methods added

--- src/commandline/TopTrumpsCLIApplication.java
+++ src/commandline/TopTrumpsCLIApplication.java
@@ -1,5 +1,7 @@
 package commandline;
 
+import java.util.Scanner;
+
 /**
  * Top Trumps command line application
  */
@@ -21,15 +23,68 @@ public class TopTrumpsCLIApplication {
 		// Loop until the user wants to exit the game
 		while (!userWantsToQuit) {
 
-			// ----------------------------------------------------
-			// Add your game logic here based on the requirements
-			// ----------------------------------------------------
+			switch(menu("Play game", "Past Game Statistics")) {
+			case 0: 
+				userWantsToQuit=true; 
+				break;
+			case 1: 
+				System.out.println("Starting game");
+				noOfAI = numberInput("Hello " + name + ", How many players do you want to play?", 1, 4);
+				break;
+			case 2:
+				System.out.println("Past Game statistics");
+				break;
+		}
 			
 			userWantsToQuit=true; // use this when the user wants to exit the game
 			
 		}
 
 
+	}
+	
+	private static int menu(String... strings) {
+		while (true) {
+			System.out.println("Please select an option");
+			for (int i = 0; i < strings.length; i++) {
+				System.out.println(i + 1 + " - " + strings[i]);
+			}
+			System.out.println("0 - Quit game");
+			Scanner sc = new Scanner(System.in);
+			try {
+				int choice = sc.nextInt();
+				if (choice < 0 || choice > strings.length) {
+					System.out.println("Invalid menu input");
+				} else {
+					return choice;
+				}
+			} catch (Exception e) {
+				System.out.println("Invalid menu input");
+			}
+		}
+	}
+	
+	public static String textInput(String question) {
+		System.out.println(question);
+		Scanner s = new Scanner(System.in);
+		return s.nextLine();
+	}
+	
+	public static int numberInput(String question, int min, int max) {
+		while (true) {
+			System.out.println(question + " [" + min + "-" + max + "]");
+			Scanner s = new Scanner(System.in);
+			try {
+				int choice = s.nextInt();
+				if (choice < min || choice > max) {
+					System.out.println("Invalid input");
+				} else {
+					return choice;
+				}
+			} catch (Exception e) {
+				System.out.println("Error: enter number");
+			}
+		}
 	}
 
 }
